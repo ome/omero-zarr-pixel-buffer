55ba9533a61961e5895a88eced4ca52bf8a2c382
Chris Allan <callan@glencoesoftware.com>
2022-04-05 15:43:28 +0100

    Inital commit

---
+++ src/main/java/com/glencoesoftware/omero/ms/core/PixelsService.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2021 Glencoe Software, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.glencoesoftware.omero.ms.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
+import org.slf4j.LoggerFactory;
+
+import ome.api.IQuery;
+import ome.io.nio.BackOff;
+import ome.io.nio.FilePathResolver;
+import ome.io.nio.PixelBuffer;
+import ome.io.nio.TileSizes;
+import ome.model.core.Image;
+import ome.model.core.Pixels;
+
+/**
+ * Subclass which overrides series retrieval to avoid the need for
+ * an injected {@link IQuery}.
+ * @author Chris Allan <callan@glencoesoftware.com>
+ *
+ */
+public class PixelsService extends ome.io.nio.PixelsService {
+
+    private static final org.slf4j.Logger log =
+            LoggerFactory.getLogger(PixelsService.class);
+
+    /** Max Tile Length */
+    private final int maxTileLength;
+
+    public PixelsService(
+            String path, boolean isReadOnlyRepo, File memoizerDirectory,
+            long memoizerWait, FilePathResolver resolver, BackOff backOff,
+            TileSizes sizes, IQuery iQuery, int maxTileLength) {
+        super(
+            path, isReadOnlyRepo, memoizerDirectory, memoizerWait, resolver,
+            backOff, sizes, iQuery
+        );
+        this.maxTileLength = maxTileLength;
+    }
+
+    /**
+     * Converts an NGFF root string to a path, initializing a {@link FileSystem}
+     * if required
+     * @param ngffDir NGFF directory root
+     * @return Fully initialized path or <code>null</code> if the NGFF root
+     * directory has not been specified in configuration.
+     * @throws IOException
+     */
+    private Path asPath(String ngffDir) throws IOException {
+        if (ngffDir.isEmpty()) {
+            return null;
+        }
+
+        try {
+            URI uri = new URI(ngffDir);
+            if ("s3".equals(uri.getScheme())) {
+                URI endpoint = new URI(
+                        uri.getScheme(), uri.getUserInfo(), uri.getHost(),
+                        uri.getPort(), "", "", "");
+                // drop initial "/"
+                String uriPath = uri.getRawPath().substring(1);
+                int first = uriPath.indexOf("/");
+                String bucket = "/" + uriPath.substring(0, first);
+                String rest = uriPath.substring(first + 1);
+                // FIXME: We might want to support additional S3FS settings in
+                // the future.  See:
+                //   * https://github.com/lasersonlab/Amazon-S3-FileSystem-NIO
+                FileSystem fs = null;
+                try {
+                    fs = FileSystems.newFileSystem(endpoint, null);
+                } catch (FileSystemNotFoundException e) {
+                    log.error("File system not found", e);
+                    return null;
+                }
+                return fs.getPath(bucket, rest);
+            }
+        } catch (URISyntaxException e) {
+            // Fall through
+        }
+        return Paths.get(ngffDir);
+    }
+
+    /**
+     * Returns a pixel buffer for a given set of pixels. Either an NGFF pixel
+     * buffer, a proprietary ROMIO pixel buffer or a specific pixel buffer
+     * implementation.
+     * @param pixels Pixels set to retrieve a pixel buffer for.
+     * @param write Whether or not to open the pixel buffer as read-write.
+     * <code>true</code> opens as read-write, <code>false</code> opens as
+     * read-only.
+     * @return A pixel buffer instance.
+     */
+    @Override
+    public PixelBuffer getPixelBuffer(Pixels pixels, boolean write) {
+        Image image = pixels.getImage();
+        String series = Integer.toString(image.getSeries());
+        try {
+            Properties properties = new Properties();
+            Path originalFilePath = Paths.get(
+                    resolver.getOriginalFilePath(this, pixels));
+            properties.load(Files.newInputStream(
+                    originalFilePath.getParent().resolve("ome_ngff.properties"),
+                    StandardOpenOption.READ
+            ));
+            Path root = asPath(properties.getProperty("uri")).resolve(series);
+            log.info("OME-NGFF root is: " + root);
+            try {
+                PixelBuffer v =
+                        new ZarrPixelBuffer(pixels, root, maxTileLength);
+                log.info("Using OME-NGFF pixel buffer");
+                return v;
+            } catch (Exception e) {
+                log.warn(
+                    "Getting OME-NGFF pixel buffer failed - " +
+                    "attempting to get local data", e);
+            }
+        } catch (IOException e1) {
+            log.debug(
+                "Failed to find OME-NGFF metadata for Image:{}", image.getId());
+        }
+        return _getPixelBuffer(pixels, write);
+    }
+
+}
+
