ce80da19e76a1e08d680372a825bd748b23582d5
Dominik Lindner <d.lindner@dundee.ac.uk>
2025-09-26 11:25:16 +0100

    Handle s3 properly

--- src/main/java/com/glencoesoftware/omero/zarr/compat/ZarrInfo.java
+++ src/main/java/com/glencoesoftware/omero/zarr/compat/ZarrInfo.java
@@ -6,15 +6,16 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.LoggerFactory;
-
 import org.apache.maven.artifact.versioning.ComparableVersion;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.bc.zarr.ZarrGroup;
 import com.google.common.base.Splitter;
 import com.upplication.s3fs.OmeroS3FilesystemProvider;
@@ -88,13 +89,30 @@ public class ZarrInfo {
             storageType = StorageType.FILE;
         } else {
             String scheme = uri.getScheme().toLowerCase();
-            if (scheme.equals("http") || scheme.equals("https")) {
+            if (scheme.startsWith("http")) {
                 storageType = StorageType.HTTP;
             } else if (scheme.equals("s3")) {
                 storageType = StorageType.S3;
             } else {
                 throw new IOException("Unsupported scheme: " + scheme);
             }
+        }
+
+        // checking for zarr v3
+        try {
+            StoreHandle sh = asStoreHandle();
+            GroupMetadata md = Group.open(sh).metadata;
+            zarrVersion = new ComparableVersion("3");  // if that works it should be v3
+            try {
+                ngffVersion = new ComparableVersion(((Map<String, Object>) md.attributes.get("ome")).get("version").toString());
+            } catch (Exception e) {
+                log.debug("Failed to get ngff version from zarr, set to 0.5");
+                ngffVersion = new ComparableVersion("0.5");
+            }
+            return;
+        } catch (Exception e) {
+            log.debug("Not zarr v3:", e);
+            // fall through
         }
 
         // checking for zarr v2
@@ -110,25 +128,7 @@ public class ZarrInfo {
             }
             return;
         } catch (Exception e) {
-            // fall through
-        }
-
-        // checking for zarr v3
-        try {
-            StoreHandle sh = asStoreHandle();
-            GroupMetadata md = Group.open(sh).metadata;
-            zarrVersion = new ComparableVersion("3");  // if that works it should be v3
-            try {
-                ngffVersion = new ComparableVersion(((Map<String, Object>) md.attributes.get("ome")).get("version").toString());
-            } catch (Exception e) {
-                log.debug("Failed to get ngff version from zarr, set to 0.5");
-                ngffVersion = new ComparableVersion("0.5");
-            }
-        } catch (Exception e) {
-            // fall through
-        }
-
-        if (zarrVersion == null) {
+            log.debug("Not zarr v2:", e);
             throw new IOException("Failed to determine zarr version");
         }
     }
@@ -232,13 +232,53 @@ public class ZarrInfo {
             String scheme = uri.getScheme() != null ? uri.getScheme().toLowerCase() : "file";
             String store = location.substring(0, location.lastIndexOf("/"));
             String zarr = location.substring(location.lastIndexOf("/")+1);
+            zarr = zarr.replaceFirst("\\?.+", "");
 
             if (scheme.startsWith("http")) {
                 return new HttpStore(store).resolve(zarr);
             }
             else if (scheme.startsWith("s3")) {
-                // TODO: implement S3Store
-                return new S3Store(null, null, null).resolve(zarr);
+                String[] tmp = store.replaceFirst("s3://", "").split("/");
+                String host = tmp[0];
+                String bucket = tmp[1];
+                String rest = String.join("/", Arrays.copyOfRange(tmp, 2, tmp.length));
+                System.out.println("Host: " + host);
+
+                // Use the OmeroS3FilesystemProvider to create AmazonS3 client
+                String query = Optional.ofNullable(uri.getQuery()).orElse("");
+                Map<String, String> params = Splitter.on('&')
+                        .trimResults()
+                        .omitEmptyStrings()
+                        .withKeyValueSeparator('=')
+                        .split(query);
+                Map<String, String> env = new HashMap<String, String>();
+                String profile = params.get("profile");
+                if (profile != null) {
+                    env.put("s3fs_credential_profile_name", profile);
+                }
+                String anonymous =
+                        Optional.ofNullable(params.get("anonymous"))
+                                .orElse("false");
+                env.put("s3fs_anonymous", anonymous);
+                OmeroS3FilesystemProvider fsp = new OmeroS3FilesystemProvider();
+                AmazonS3 client = fsp.createAmazonS3(uri, env);
+
+                // Create s3 client manually:
+                // AmazonS3 client = AmazonS3ClientBuilder.standard()
+                // .withEndpointConfiguration(new EndpointConfiguration(host, null))
+                // .withCredentials(new AWSCredentialsProvider() {
+                //     @Override
+                //     public AWSCredentials getCredentials() {
+                //         return new AnonymousAWSCredentials();
+                //     }
+
+                //     @Override
+                //     public void refresh() {
+                //         // do nothing
+                //     }
+                // })
+                // .build();
+                return new S3Store(client, bucket, rest).resolve(zarr);
             }
             else {
                 return new FilesystemStore(store).resolve(zarr);
