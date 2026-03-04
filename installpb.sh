67fd5a379dc26fd39ed3cfa78dfad8f3900f3072
Dominik Lindner <d.lindner@dundee.ac.uk>
2026-03-04 15:20:28 +0000

    fix install scripts

--- installpb.sh
+++ installpb.sh
@@ -7,7 +7,13 @@ OZPB=https://artifacts.glencoesoftware.com/artifactory/gs-omero-snapshots-local/
 rm -f $OMERO_DIST/lib/server/jzarr.jar
 
 # Install omero-zarr-pixel-buffer
-wget -P $OMERO_DIST/lib/server $OZPB
+if [ -z "${OZPB:-}" ]; then
+    echo "OZPB is not set; you have to manually copy the omero-zarr-pixel-buffer jar to $OMERO_DIST/lib/server"
+else
+    wget -P $OMERO_DIST/lib/server $OZPB
+fi
+
+# Install dependencies
 wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/com/github/ben-manes/caffeine/caffeine/3.1.8/caffeine-3.1.8.jar
 wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/dev/zarr/jzarr/0.4.2/jzarr-0.4.2.jar
 wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/org/lasersonlab/s3fs/2.2.3/s3fs-2.2.3.jar
--- installpbv3.sh
+++ installpbv3.sh
@@ -1,25 +1,23 @@
 #/bin/bash
 
-# copy the build jar over first
-# scp /Users/dom/Repos/omero-zarr-pixel-buffer/build/libs/omero-zarr-pixel-buffer-0.6.2-SNAPSHOT.jar ansible@omero.home:/tmp/
-
 OMERO_DIST=/opt/omero/server/OMERO.server
-OZPB=/tmp/omero-zarr-pixel-buffer-0.6.2-SNAPSHOT.jar
+OZPB=
 
-awsSdkVersion=2.34.6
 jacksonVersion=2.20.0
-logbackVersion=1.3.14
+awsSdkVersion=2.34.6
 zarrJavaVersion=0.0.10
-formatsVersion=8.3.0
+
+# Remove older installation
+rm -f $OMERO_DIST/lib/server/omero-zarr-pixel-buffer*.jar $OMERO_DIST/lib/server/caffeine*.jar $OMERO_DIST/lib/server/jzarr*.jar $OMERO_DIST/lib/server/s3fs*.jar $OMERO_DIST/lib/server/aws-java-*.jar
 
 # Install omero-zarr-pixel-buffer
-mv $OZPB $OMERO_DIST/lib/server
+if [ -z "${OZPB:-}" ]; then
+    echo "OZPB is not set; you have to manually copy the omero-zarr-pixel-buffer jar to $OMERO_DIST/lib/server"
+else
+    wget -P $OMERO_DIST/lib/server $OZPB
+fi
 
-# Remove older jars
-cd $OMERO_DIST/lib/server
-rm -rf aws-java-sdk-* jackson-annotations.jar jackson-core.jar jackson-databind.jar logback-classic.jar logback-core.jar omero-zarr-pixel-buffer-* s3fs-2.2.3.jar caffeine-3.1.8.jar jzarr-0.4.2.jar
-
-# Get all the dependancies
+# Install dependencies
 wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/software/amazon/awssdk/s3/${awsSdkVersion}/s3-${awsSdkVersion}.jar
 wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/software/amazon/awssdk/auth/${awsSdkVersion}/auth-${awsSdkVersion}.jar
 wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/software/amazon/awssdk/aws-core/${awsSdkVersion}/aws-core-${awsSdkVersion}.jar
@@ -32,19 +30,10 @@ wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/software/amazon/aw
 wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/software/amazon/awssdk/retries/${awsSdkVersion}/retries-${awsSdkVersion}.jar
 wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/software/amazon/awssdk/url-connection-client/${awsSdkVersion}/url-connection-client-${awsSdkVersion}.jar
 
-wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/${jacksonVersion}/jackson-core-${jacksonVersion}.jar
-wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/${jacksonVersion}/jackson-databind-${jacksonVersion}.jar
-wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/fasterxml/jackson/datatype/jackson-datatype-jdk8/${jacksonVersion}/jackson-datatype-jdk8-${jacksonVersion}.jar
-wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.20/jackson-annotations-2.20.jar
-
-wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/ch/qos/logback/logback-classic/${logbackVersion}/logback-classic-${logbackVersion}.jar
-wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/ch/qos/logback/logback-core/${logbackVersion}/logback-core-${logbackVersion}.jar
-wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/dev/zarr/zarr-java/${zarrJavaVersion}/zarr-java-${zarrJavaVersion}.jar
-
 wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/com/github/ben-manes/caffeine/caffeine/3.1.8/caffeine-3.1.8.jar
-wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/org/apache/tika/tika-core/1.28.5/tika-core-1.28.5.jar
-wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/squareup/okhttp/okhttp/2.7.5/okhttp-2.7.5.jar
-wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/github/luben/zstd-jni/1.5.5-7/zstd-jni-1.5.5-7.jar
-wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/scalableminds/blosc-java/0.1-1.21.4/blosc-java-0.1-1.21.4.jar
 
-chown -R omero-server:omero-server -P $OMERO_DIST/lib/server
+wget -P $OMERO_DIST/lib/server https://repo.maven.apache.org/maven2/dev/zarr/zarr-java/${zarrJavaVersion}/zarr-java-${zarrJavaVersion}.jar
+wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/scalableminds/blosc-java/0.1-1.21.4/blosc-java-0.1-1.21.4.jar
+wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/squareup/okhttp/okhttp/2.7.5/okhttp-2.7.5.jar
+
+wget -P $OMERO_DIST/lib/server https://repo1.maven.org/maven2/com/fasterxml/jackson/datatype/jackson-datatype-jdk8/${jacksonVersion}/jackson-datatype-jdk8-${jacksonVersion}.jar
